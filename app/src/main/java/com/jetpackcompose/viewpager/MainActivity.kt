24bffb6a07aee6daef677d4da4c25825bfecac47
saqib <saqib@saqib.com>
2023-04-09 13:52:02 +0200

    view pager container screen

--- app/src/main/java/com/jetpackcompose/viewpager/MainActivity.kt
+++ app/src/main/java/com/jetpackcompose/viewpager/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.jetpackcompose.viewpager.screens.ViewPagerScreen
 import com.jetpackcompose.viewpager.ui.theme.JetpackComposeViewPagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   ViewPagerScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeViewPagerTheme {
-        Greeting("Android")
     }
 }
